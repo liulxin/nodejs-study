75f303e032ace780d9f653c6c547ff6bea508029
dongwudi <550293874@qq.com>
2019-10-23 00:16:30 +0800

    node mysql

--- node-03/mysql.js
+++ node-03/mysql.js
@@ -1,4 +1,4 @@
-(async () => {
+;(async () => {
   const mysql = require('mysql2/promise')
 
   //config
@@ -11,5 +11,38 @@
 
   //create the connection
   const connection = await mysql.createConnection(cfg)
-  console.log(connection)
+
+  connection.connect()
+
+  // 创建表
+  connection.query(
+    `CREATE TABLE IF NOT EXISTS test(
+    id INT NOT NULL AUTO_INCREMENT,
+    message VARCHAR(45) NULL,
+    PRIMARY KEY(id)
+  )`,
+    (err, res) => {
+      console.log(res)
+    }
+  )
+
+  // 插入语句
+  connection.query(`INSERT INTO test(message) VALUES(?)`,('learn mysql nodejs'), (err, res) => {
+    console.log(res)
+  })
+
+  // 查询
+  connection.query(`SELECT * FROM test`, (err, res) => {
+    console.log(res)
+  })
+
+  // 更新
+  connection.query(`UPDATE test SET message = ? WHERE id = 9`,'this is 9 mod', (err, res) => {
+    console.log(res)
+  })
+
+  // 删除
+  connection.query(`DELETE FROM test where id < 8`,(err, res) => {
+    console.log(res)
+  })
 })()
